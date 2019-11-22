0af05b57253b8c2e2ef873d6b3e0fc2d5493ce4a
DeniHidayatullah <31883814+DeniHidayatullah@users.noreply.github.com>
2019-11-22 12:36:32 +0700

    raport baru
    raport baru

--- application/raport/raport_halaman_guru.php
+++ application/raport/raport_halaman_guru.php
@@ -1,22 +1,26 @@
-            <div class="col-xs-12">  
+            <div class="col-xs-12">
               <div class="box">
                 <div class="box-header">
-                  <h3 class="box-title"><?php if (isset($_GET[tahun])){ echo "Input Nilai Raport Siswa"; }else{ echo "Input Nilai Raport Siswa ".date('Y'); } ?></h3>
+                  <h3 class="box-title"><?php if (isset($_GET[tahun])) {
+                                          echo "Input Nilai Raport Siswa";
+                                        } else {
+                                          echo "Input Nilai Raport Siswa " . date('Y');
+                                        } ?></h3>
                   <form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
                     <input type="hidden" name='view' value='raport'>
                     <input type="hidden" name='act' value='detailguru'>
                     <select name='tahun' style='padding:4px'>
-                        <?php 
-                            echo "<option value=''>- Pilih Tahun Akademik -</option>";
-                            $tahun = mysqli_query($koneksi,"SELECT * FROM tahun_akademik");
-                            while ($k = mysqli_fetch_array($tahun)){
-                              if ($_GET[tahun]==$k[id_tahun_akademik]){
-                                echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
-                              }else{
-                                echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
-                              }
-                            }
-                        ?>
+                      <?php
+                      echo "<option value=''>- Pilih Tahun Akademik -</option>";
+                      $tahun = mysqli_query($koneksi, "SELECT * FROM tahun_akademik");
+                      while ($k = mysqli_fetch_array($tahun)) {
+                        if ($_GET[tahun] == $k[id_tahun_akademik]) {
+                          echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
+                        } else {
+                          echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
+                        }
+                      }
+                      ?>
                     </select>
                     <input type="submit" style='margin-top:-4px' class='btn btn-success btn-sm' value='Lihat'>
                   </form>
@@ -35,38 +39,35 @@
                         <th>Selesai</th>
                         <th>Ruangan</th>
                         <th>Semester</th>
-                        <?php 
-                          if (isset($_GET[tahun])){
-                            echo "<th>Action</th>";
-                          }
+                        <?php
+                        if (isset($_GET[tahun])) {
+                          echo "<th>Action</th>";
+                        }
                         ?>
                       </tr>
                     </thead>
                     <tbody>
-                  <?php
-                    if (isset($_GET[tahun])){
-                      $tampil = mysqli_query($koneksi,"SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, b.kode_kurikulum, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
+                      <?php
+                      if (isset($_GET[tahun])) {
+                        $tampil = mysqli_query($koneksi, "SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
                                             JOIN mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
                                               JOIN guru c ON a.nip=c.nip 
                                                 JOIN ruangan d ON a.kode_ruangan=d.kode_ruangan
                                                   JOIN kelas e ON a.kode_kelas=e.kode_kelas 
                                                   where a.nip='$_SESSION[id]' 
-                                                    AND a.id_tahun_akademik='$_GET[tahun]' 
-                                                      AND b.kode_kurikulum='$kurikulum[kode_kurikulum]' ORDER BY a.hari DESC");
-                    
-                    }else{
-                      $tampil = mysqli_query($koneksi,"SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, b.kode_kurikulum, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
+                                                    AND a.id_tahun_akademik='$_GET[tahun]' ORDER BY a.hari DESC");
+                      } else {
+                        $tampil = mysqli_query($koneksi, "SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
                                             JOIN mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
                                               JOIN guru c ON a.nip=c.nip 
                                                 JOIN ruangan d ON a.kode_ruangan=d.kode_ruangan
                                                 JOIN kelas e ON a.kode_kelas=e.kode_kelas 
                                                   where a.nip='$_SESSION[id]' 
-                                                    AND b.kode_kurikulum='$kurikulum[kode_kurikulum]'
-                                                      AND a.id_tahun_akademik LIKE '".date('Y')."%' ORDER BY a.hari DESC");
-                    }
-                    $no = 1;
-                    while($r=mysqli_fetch_array($tampil)){
-                    echo "<tr><td>$no</td>
+                                                      AND a.id_tahun_akademik LIKE '" . date('Y') . "%' ORDER BY a.hari DESC");
+                      }
+                      $no = 1;
+                      while ($r = mysqli_fetch_array($tampil)) {
+                        echo "<tr><td>$no</td>
                               <td>$r[namamatapelajaran]</td>
                               <td>$r[nama_kelas]</td>
                               <td>$r[nama_guru]</td>
@@ -75,20 +76,20 @@
                               <td>$r[jam_selesai]</td>
                               <td>$r[nama_ruangan]</td>
                               <td>$r[id_tahun_akademik]</td>";
-                              if (isset($_GET[tahun])){
-                                echo "<td style='width:255px !important'><center>
+                        if (isset($_GET[tahun])) {
+                          echo "<td style='width:255px !important'><center>
                                           <a class='btn btn-warning btn-xs' title='Lihat Nilai Sikap Siswa' href='index.php?view=raport&act=listsiswasikap&jdwl=$r[kodejdwl]&kd=$r[kode_pelajaran]&id=$r[kode_kelas]&tahun=$_GET[tahun]'><span class='glyphicon glyphicon-th-list'></span> Sikap</a>
                                           <a class='btn btn-success btn-xs' title='Lihat Nilai Pengetahuan Siswa' href='index.php?view=raport&act=listsiswa&jdwl=$r[kodejdwl]&kd=$r[kode_pelajaran]&id=$r[kode_kelas]&tahun=$_GET[tahun]'><span class='glyphicon glyphicon-th-list'></span> Pengetahuan</a>
                                           <a class='btn btn-primary btn-xs' title='Lihat Nilai Keterampilan Siswa' href='index.php?view=raport&act=listsiswaketerampilan&jdwl=$r[kodejdwl]&kd=$r[kode_pelajaran]&id=$r[kode_kelas]&tahun=$_GET[tahun]'><span class='glyphicon glyphicon-th-list'></span> Keterampilan</a>
                                         </center></td>";
-                              }
-                          
-                          echo "</tr>";
-                      $no++;
+                        }
+
+                        echo "</tr>";
+                        $no++;
                       }
-                  ?>
+                      ?>
                     </tbody>
                   </table>
                 </div><!-- /.box-body -->
-                </div>
+              </div>
             </div>
--- application/raport/raport_uts_halaman_guru.php
+++ application/raport/raport_uts_halaman_guru.php
@@ -1,22 +1,26 @@
-            <div class="col-xs-12">  
+            <div class="col-xs-12">
               <div class="box">
                 <div class="box-header">
-                  <h3 class="box-title"><?php if (isset($_GET[tahun])){ echo "Input Nilai Raport UTS"; }else{ echo "Input Nilai Raport UTS Anda ".date('Y'); } ?></h3>
+                  <h3 class="box-title"><?php if (isset($_GET[tahun])) {
+                                          echo "Input Nilai Raport UTS";
+                                        } else {
+                                          echo "Input Nilai Raport UTS Anda " . date('Y');
+                                        } ?></h3>
                   <form style='margin-right:5px; margin-top:0px' class='pull-right' action='' method='GET'>
                     <input type="hidden" name='view' value='raportuts'>
                     <input type="hidden" name='act' value='detailguru'>
                     <select name='tahun' style='padding:4px'>
-                        <?php 
-                            echo "<option value=''>- Pilih Tahun Akademik -</option>";
-                            $tahun = mysqli_query($koneksi,"SELECT * FROM tahun_akademik");
-                            while ($k = mysqli_fetch_array($tahun)){
-                              if ($_GET[tahun]==$k[id_tahun_akademik]){
-                                echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
-                              }else{
-                                echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
-                              }
-                            }
-                        ?>
+                      <?php
+                      echo "<option value=''>- Pilih Tahun Akademik -</option>";
+                      $tahun = mysqli_query($koneksi, "SELECT * FROM tahun_akademik");
+                      while ($k = mysqli_fetch_array($tahun)) {
+                        if ($_GET[tahun] == $k[id_tahun_akademik]) {
+                          echo "<option value='$k[id_tahun_akademik]' selected>$k[nama_tahun]</option>";
+                        } else {
+                          echo "<option value='$k[id_tahun_akademik]'>$k[nama_tahun]</option>";
+                        }
+                      }
+                      ?>
                     </select>
                     <input type="submit" style='margin-top:-4px' class='btn btn-success btn-sm' value='Lihat'>
                   </form>
@@ -35,38 +39,35 @@
                         <th>Selesai</th>
                         <th>Ruangan</th>
                         <th>Semester</th>
-                        <?php 
-                          if (isset($_GET[tahun])){
-                            echo "<th>Action</th>";
-                          }
+                        <?php
+                        if (isset($_GET[tahun])) {
+                          echo "<th>Action</th>";
+                        }
                         ?>
                       </tr>
                     </thead>
                     <tbody>
-                  <?php
-                    if (isset($_GET[tahun])){
-                      $tampil = mysqli_query($koneksi,"SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, b.kode_kurikulum, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
+                      <?php
+                      if (isset($_GET[tahun])) {
+                        $tampil = mysqli_query($koneksi, "SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
                                             JOIN mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
                                               JOIN guru c ON a.nip=c.nip 
                                                 JOIN ruangan d ON a.kode_ruangan=d.kode_ruangan
                                                   JOIN kelas e ON a.kode_kelas=e.kode_kelas 
                                                   where a.nip='$_SESSION[id]' 
-                                                    AND a.id_tahun_akademik='$_GET[tahun]' 
-                                                      AND b.kode_kurikulum='$kurikulum[kode_kurikulum]' ORDER BY a.hari DESC");
-                    
-                    }else{
-                      $tampil = mysqli_query($koneksi,"SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, b.kode_kurikulum, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
+                                                    AND a.id_tahun_akademik='$_GET[tahun]'  ORDER BY a.hari DESC");
+                      } else {
+                        $tampil = mysqli_query($koneksi, "SELECT a.*, e.nama_kelas, b.namamatapelajaran, b.kode_pelajaran, c.nama_guru, d.nama_ruangan FROM jadwal_pelajaran a 
                                             JOIN mata_pelajaran b ON a.kode_pelajaran=b.kode_pelajaran
                                               JOIN guru c ON a.nip=c.nip 
                                                 JOIN ruangan d ON a.kode_ruangan=d.kode_ruangan
                                                 JOIN kelas e ON a.kode_kelas=e.kode_kelas 
                                                   where a.nip='$_SESSION[id]' 
-                                                    AND b.kode_kurikulum='$kurikulum[kode_kurikulum]'
-                                                      AND a.id_tahun_akademik LIKE '".date('Y')."%' ORDER BY a.hari DESC");
-                    }
-                    $no = 1;
-                    while($r=mysqli_fetch_array($tampil)){
-                    echo "<tr><td>$no</td>
+                                                      AND a.id_tahun_akademik LIKE '" . date('Y') . "%' ORDER BY a.hari DESC");
+                      }
+                      $no = 1;
+                      while ($r = mysqli_fetch_array($tampil)) {
+                        echo "<tr><td>$no</td>
                               <td>$r[namamatapelajaran]</td>
                               <td>$r[nama_kelas]</td>
                               <td>$r[nama_guru]</td>
@@ -75,18 +76,18 @@
                               <td>$r[jam_selesai]</td>
                               <td>$r[nama_ruangan]</td>
                               <td>$r[id_tahun_akademik]</td>";
-                              if (isset($_GET[tahun])){
-                                echo "<td style='width:70px !important'><center>
+                        if (isset($_GET[tahun])) {
+                          echo "<td style='width:70px !important'><center>
                                           <a class='btn btn-success btn-xs' title='Lihat Siswa' href='index.php?view=raportuts&act=listsiswa&jdwl=$r[kodejdwl]&kd=$r[kode_pelajaran]&id=$r[kode_kelas]&tahun=$_GET[tahun]'><span class='glyphicon glyphicon-th-list'></span> Input Nilai</a>
                                         </center></td>";
-                              }
-                          
-                          echo "</tr>";
-                      $no++;
+                        }
+
+                        echo "</tr>";
+                        $no++;
                       }
-                  ?>
+                      ?>
                     </tbody>
                   </table>
                 </div><!-- /.box-body -->
-                </div>
+              </div>
             </div>
