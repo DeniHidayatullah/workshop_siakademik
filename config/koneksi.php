1c74612217d7d259b522a4434ba61feedc6d2e09
DeniHidayatullah <31883814+DeniHidayatullah@users.noreply.github.com>
2019-12-05 08:26:02 +0700

    update koneksi
    edit koneksi'

--- config/koneksi.php
+++ config/koneksi.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Jakarta');
 $server = "localhost";
 $username = "root";
 $password = "";
-$database = "smk7";
+$database = "workshop_siakademik";
 
 $koneksi = mysqli_connect($server, $username, $password, $database);
 mysqli_connect($server, $username, $password, $database);
